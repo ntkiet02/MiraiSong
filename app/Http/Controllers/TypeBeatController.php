e10577f4aec702f9b9217204c1d173cfc61c8ceb
ntrkiet02 <ntrkiet@gmail.com>
2023-12-01 13:39:08 +0700

    upload Laravel project

---
+++ app/Http/Controllers/TypeBeatController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TypeBeat;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class TypeBeatController extends Controller
+{
+    public function getList()
+    {
+        $typebeat = TypeBeat::all();
+        return view('typebeat.list', compact('typebeat'));
+    }
+    public function getAdd()
+    {
+        return view('typebeat.add');
+    }  
+    public function postAdd(Request $request)
+    {
+        $orm = new TypeBeat();
+        $orm->typename = $request->typename;
+        $orm->typename_slug = Str::slug($request->typename, '-');
+        $orm->save();
+
+        return redirect()->route('typebeat');
+    }    public function getUpdate($id)
+    {
+        $typebeat = TypeBeat::find($id);
+        return view('typebeat.update', compact('typebeat'));
+    }
+    public function postUpdate(Request $request, $id)
+    {
+        $orm = TypeBeat::find($id);
+        $orm->typename = $request->typename;
+        $orm->typename_slug = Str::slug($request->typename, '-');
+        $orm->save();
+        
+        return redirect()->route('typebeat');
+    }  
+    public function getDelete($id)
+    {
+        $orm = TypeBeat::find($id);
+        $orm->delete();
+        return redirect()->route('typebeat');
+    }
+}
