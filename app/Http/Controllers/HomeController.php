3fd8fb5350d5704d5a27760ddc8e068273b628d2
ntrkiet02 <ntrkiet@gmail.com>
2023-12-06 17:10:41 +0700

    Command dong code yeu cau dang nhap

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,12 +10,13 @@ class HomeController extends Controller
      * Create a new controller instance.
      *
      * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
+     */ 
+    
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+        // Code trên yêu cầu đăng nhập
     /**
      * Show the application dashboard.
      *
