8d550deaae25fa7bcd4a34132c9c8a8d9b3114d6
ntrkiet02 <ntrkiet@gmail.com>
2023-12-10 16:01:44 +0700

    ???

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,23 +1,124 @@
 @extends('layouts.app')
-
+@section('title','Trang chủ')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Trang chủ</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+<!-- Breadcrumb Begin -->
+<div class="breadcrumb-option spad set-bg" data-setbg="resources/img/1.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="breadcrumb__text">
+                        <h2>Our Sevices</h2>
+                        <div class="breadcrumb__links">
+                            <a href="#">Home</a>
+                            <span>Services</span>
                         </div>
-                    @endif
-
-                    Trang chủ cho khác truy cập
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    <!-- Breadcrumb End -->
+
+    <!-- Services Section Begin -->
+    <section class="services-page spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="services__item">
+                        <div class="services__item__icon">
+                            <img src="img/icons/si-2.png" alt="">
+                        </div>
+                        <h4>Scriptwriting and editing</h4>
+                        <p>Whether you’re halfway through the editing process, or you haven’t even started, our post
+                            production services can put the finishing touches.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="services__item">
+                        <div class="services__item__icon">
+                            <img src="img/icons/si-1.png" alt="">
+                        </div>
+                        <h4>Motion graphics</h4>
+                        <p>Whether you’re halfway through the editing process, or you haven’t even started, our post
+                            production services can put the finishing touches.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="services__item">
+                        <div class="services__item__icon">
+                            <img src="img/icons/si-2.png" alt="">
+                        </div>
+                        <h4>Scriptwriting and editing</h4>
+                        <p>Whether you’re halfway through the editing process, or you haven’t even started, our post
+                            production services can put the finishing touches.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="services__item">
+                        <div class="services__item__icon">
+                            <img src="img/icons/si-3.png" alt="">
+                        </div>
+                        <h4>Video distribution</h4>
+                        <p>Whether you’re halfway through the editing process, or you haven’t even started, our post
+                            production services can put the finishing touches.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="services__item">
+                        <div class="services__item__icon">
+                            <img src="img/icons/si-4.png" alt="">
+                        </div>
+                        <h4>Video hosting</h4>
+                        <p>Whether you’re halfway through the editing process, or you haven’t even started, our post
+                            production services can put the finishing touches.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="services__item">
+                        <div class="services__item__icon">
+                            <img src="img/icons/si-4.png" alt="">
+                        </div>
+                        <h4>Video hosting</h4>
+                        <p>Whether you’re halfway through the editing process, or you haven’t even started, our post
+                            production services can put the finishing touches.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Services Section End -->
+
+    <!-- Call To Action Section Begin -->
+    <section class="callto sp__callto">
+        <div class="container">
+            <div class="callto__services spad set-bg" data-setbg="img/calltos-bg.jpg">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-lg-10 text-center">
+                        <div class="callto__text">
+                            <h2>CREATE AWESOME VIDEOS WITH WIDEO’S POWERFUL FEATURES</h2>
+                            <p>Wideo combines all the features you need to easily create professional videos and
+                                presentations</p>
+                            <a href="#">Start your stories</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Call To Action Section End -->
+
+    <!-- Logo Begin -->
+    <div class="logo spad">
+        <div class="container">
+            <div class="logo__carousel owl-carousel">
+                <a href="#" class="logo__item"><img src="img/logo/logo-1.png" alt=""></a>
+                <a href="#" class="logo__item"><img src="img/logo/logo-2.png" alt=""></a>
+                <a href="#" class="logo__item"><img src="img/logo/logo-3.png" alt=""></a>
+                <a href="#" class="logo__item"><img src="img/logo/logo-4.png" alt=""></a>
+                <a href="#" class="logo__item"><img src="img/logo/logo-5.png" alt=""></a>
+                <a href="#" class="logo__item"><img src="img/logo/logo-6.png" alt=""></a>
+            </div>
+        </div>
+    </div>
+    <!-- Logo End -->
 @endsection
