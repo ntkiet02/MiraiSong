0beda5592e957382564238eb817fc5b1dcca37a4
HanBui10 <buih72436@gmail.com>
2024-01-05 19:20:10 +0700

    update 403

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -5,8 +5,7 @@
 		<div class="notfound">
 			<div class="notfound-403">
 				<h1>403</h1>
-				<h2>Không Được Phép</h2>
-                <h3 class="h5 fw-normal mb-4">Tài khoản không đủ quyền truy cập chức năng này.</h3>
+				<h2>Không Được Phép Truy Cập</h2>
 			</div>
 			<br><a href="{{ route('frontend.home') }}">Home</a></br>
 		</div>
